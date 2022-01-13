6355f190714ae4347c7de4e75ef3a2c10866a5cf
Juanma <juanma_trelles@hotmail.com>
2022-01-13 14:08:13 -0300

    Fixed bugs

--- foo.R
+++ foo.R
@@ -1,17 +1,16 @@
 # Radius
 r <- 2
 # Function to compute the volume of a sphere with radius r
-volume <- function(r, rho) {
+volume <- function(r) {
   3/4*pi*r^2
 }
-# Function to compute the volumes of the spheres with radius r, r^2 and
-r^3
-volume_vector <- function(r) {
-  r <- 22
-  for (r in 2:4){
-    volume(r)
+volume(r)
+# Function to compute the volumes of the spheres with radius r, r^2 and r^3
+volume_vector <- function(x) {
+  for (b in 1:3){
+  print(volume(r^b))
   }
 }
-# Run volume_vector(r) and print the volumes of the spheres with radius
-r, r^2 and r^3
+# Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3
 volume_vector(r)
+
